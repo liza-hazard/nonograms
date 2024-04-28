34649a86d7595e8754cc5348ed86d4e15a3e7a52
Liza Dorozhynska <liza.hazard20@gmail.com>
2024-04-28 12:30:05 +0100

    feat: create class for nonograms

--- scripts.js
+++ scripts.js
@@ -21,17 +21,70 @@ const testPicture = {
     ]
 }
 
+class Nonogram {
+    rows;
+    cols;
+    complexity;
+    constructor(complexityNum, coordinates) {
+        switch (complexityNum) {
+            case 1:
+                this.complexity = 'light'
+                this.rows = 5;
+                this.cols = 5;
+                break;
+            case 2:
+                this.complexity = 'medium'
+                this.rows = 10;
+                this.cols = 10;
+                break;
+            case 3:
+                this.complexity = 'hard'
+                this.rows = 15;
+                this.cols = 15;
+                break;
+        }
+        this.coordinates = coordinates;
+    }
+    get size() {
+        return this.rows * this.cols;
+    }
+    victoryBoxes() {
+        const victory = []
+        for (let arr = 0; arr < this.coordinates.length; arr++) {
+            console.log()
+            this.coordinates[arr].forEach((el, index) => {
+                if (el) {
+                    victory.push(index + (arr * this.cols))
+                }
+            })
+        }
+        return victory;
+    }
+    
+
+}
+
+let pic = new Nonogram(1, [
+    [1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1]
+]);
+
+
 function gameStart(picture) {
     let nonogramBlock = document.createElement('div')
     nonogramBlock.classList.add('nonogram')
-    nonogramBlock.setAttribute('data-size', picture.size())
-    for (let i = 0; i < picture.size(); i++) {
+    nonogramBlock.setAttribute('data-size', picture.size)
+    for (let i = 0; i < picture.size; i++) {
         let box = document.createElement('div')
         box.classList.add('box')
         box.addEventListener('click', () => {
             if (!box.classList.contains('nofill')) {
                 box.classList.toggle('filled')
             }
+            
         })
         box.addEventListener('contextmenu', (e) => {
             e.preventDefault()
@@ -46,7 +99,8 @@ function gameStart(picture) {
 }
 
 
-gameStart(testPicture)
+
+gameStart(pic)
 
 /***/ })
 
